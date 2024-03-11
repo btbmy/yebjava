f5175b25274aee0db6fb32f010c06a83397969b6
btbmy <102711653+btbmy@users.noreply.github.com>
2024-03-11 23:58:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-''}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
 export NEZHA_KEY=${NEZHA_KEY:-''}
 export TLS=${TLS:-'1'}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
-export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
+export WEB_DOMAIN=${WEB_DOMAIN:-'exle.com'}
 export ARGO_AUTH=${ARGO_AUTH:-''}
-export CFIP=${CFIP:-'icook.hk'}
+export CFIP=${CFIP:-'t.t'}
 export NAME=${NAME:-''}
 export SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
 
